3c2a789d331206cb9a0c32a353849d4c9e5787de
damineesaini <damineesaini11@gmail.com>
2019-04-03 09:23:25 +0530

    dd

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Video;
 use App\Http\Controllers\Controller;
+use DB;
+use App\Search;
 
 class MainController extends Controller
 {
     public function index() {
          $videos = Video::all();
-        return view('main.index')->with('videos',$videos);
+         $search=Search::whereIn('c_id',[1,2,3])->get();
+        return view('main.index',compact('search','videos'));
 //dd('hello');
         /*$videos = Video::orderBy('id','desc')->first();
         dd(json_decode($videos->upload)[0]->download_link);
@@ -23,5 +26,7 @@ class MainController extends Controller
                         ->first();
 
         return view('video.video')->with('video',$video);
+
+        
     }
 }
