7a6200e8a897ffb0205c2ad748118c1045548993
puneet singh <singhpuneei@gmail.com>
2019-04-04 17:23:31 +0930

    Changes by Puneet

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -13,9 +13,22 @@
                   <button class="btn btn-light" type="button">
                   <i class="fas fa-search"></i> 
                   </button>
+
                </div>
+
             </div>
+            
         </div>
+<div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
     </form>
 
     @if (Route::has('login'))
