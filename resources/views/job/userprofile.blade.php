47bd16118afc9935c7707a22e6708a2e8190f678
damineesaini <damineesaini11@gmail.com>
2019-08-13 12:01:17 +0530

    ch

--- resources/views/job/userprofile.blade.php
+++ resources/views/job/userprofile.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 @push('topscript')
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 @endpush
 
 @section('center-content')
@@ -14,25 +13,41 @@
     <div class="col-sm-10">
         <div data-spy="scroll" class="tabbable-panel">
             <div class="tabbable-line">
-                <ul class="nav nav-tabs ">
-                    <li class="active">
+                <!-- List group -->
+<div class="list-group list-group-horizontal-md " id="myList" role="tablist">
+  <a class="list-group-item list-group-item-action active" data-toggle="list" href="#Personalinfo" role="tab">Personal Info</a>
+  <a class="list-group-item list-group-item-action" data-toggle="list" href="#Education" role="tab">Education</a>
+  <a class="list-group-item list-group-item-action" data-toggle="list" href="#Work" role="tab">Work Experience</a>
+  <a class="list-group-item list-group-item-action" data-toggle="list" href="#Certificates" role="tab">Certification Details</a>
+  <a class="list-group-item list-group-item-action" data-toggle="list" href="#Resume" role="tab">Resume</a>
+</div>
+
+ <!-- Tab panes -->
+{{--<div class="tab-content">
+  <div class="tab-pane active" id="home" role="tabpanel">Daminee</div>
+  <div class="tab-pane" id="profile" role="tabpanel">...</div>
+  <div class="tab-pane" id="messages" role="tabpanel">...</div>
+  <div class="tab-pane" id="settings" role="tabpanel">...</div>
+</div> --}}
+               {{--  <ul class="nav nav-tabs ">
+                    <li class="nav-item active">
                         <a href="#tab_default_1" data-toggle="tab">Personal Info </a>
                     </li>
-                    <li>
+                    <li class="nav-item">
                         <a href="#tab_default_2" data-toggle="tab">Education</a>
                     </li>
-                    <li>
+                    <li class="nav-item">
                         <a href="#tab_default_3" data-toggle="tab">Work Experience</a>
                     </li>
-                    <li>
+                    <li class="nav-item">
                         <a href="#tab_default_5" data-toggle="tab">Certification Details</a>
                     </li>
-                    <li>
+                    <li class="nav-item">
                         <a href="#tab_default_6" data-toggle="tab">Resume</a>
                     </li>
-                </ul>
+                </ul> --}}
                 <div class="tab-content">
-                    <div class="tab-pane active" id="tab_default_1">
+                    <div class="tab-pane active" id="Personalinfo">
                         <div class="well well-sm">
                             <h4>PERSONAL DATA</h4>
                         </div>
@@ -73,7 +88,7 @@
                         </tbody>
                     </table>
                 </div>         
-                <div class="tab-pane" id="tab_default_2">
+                <div class="tab-pane" id="Education">
                     <div class="well well-sm">
                         <h4>EDUCATIONAL BACKGROUND</h4>
                     </div>
@@ -96,7 +111,7 @@
                     </table>
                 </div>
                 
-                <div class="tab-pane" id="tab_default_3">
+                <div class="tab-pane" id="Work">
                     <div class="well well-sm">
                         <h4>EXPERIENCE</h4>
                     </div>
@@ -135,7 +150,7 @@
                     </table>
                 </div>
                  
-                <div class="tab-pane" id="tab_default_5">
+                <div class="tab-pane" id="Certificates">
                     <div class="well well-sm">
                         <h4>CERTIFICATION DETAILS</h4>
                     </div>
@@ -176,7 +191,7 @@
                     
                 </div>
                 
-                <div class="tab-pane" id="tab_default_6">
+                <div class="tab-pane" id="Resume">
                     <div class="well well-sm">
                         <h4>Download Resume</h4>
                     </div>
